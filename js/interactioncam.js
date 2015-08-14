a380f65edd1394f227aa26f87aed6f48457d8f3e
Sohil Patel <sohilpatel4932@gmail.com>
2015-08-14 14:26:04 +0530

    New For indépendant day

--- js/interactioncam.js
+++ js/interactioncam.js
@@ -10,7 +10,7 @@
       
  var ctx    = canvas.getContext('2d'),
      streaming    = false,
-     width  = 600,
+     width  = 640,
      height = 450,
      state  = 'intro';
 
@@ -23,9 +23,9 @@
 
   /* BRANDING */
   var img = new Image(),
-      imgwidth = 150,
-      imgheight = 150;
-  img.src = 'icon.png';
+      imgwidth = 640,
+      imgheight = 450;
+  img.src = 'zando_new.png';
 
   var email = new Text();
 
@@ -70,7 +70,9 @@
     ctx.drawImage(video, 0, 0, width, finalheight);
     ctx.restore();
     ctx.scale(1, 1);
-    ctx.drawImage(img, 590 - imgwidth, 440 - imgheight, imgwidth, imgheight);
+    ctx.globalAlpha = 0.6;
+    ctx.drawImage(img, 0, 0, width, finalheight);
+    ctx.globalAlpha = 1;
   }
 
   function reshoot() {
@@ -80,50 +82,20 @@
     if (state === 'reviewing' || state === 'uploaded') {
       canvas.width = width;
       canvas.height = finalheight;
-      ctx.drawImage(img, 590 - imgwidth, 440 - imgheight, imgwidth, imgheight);
+
+      ctx.globalAlpha = 0.6;
+      ctx.drawImage(img, 0, 0, width, finalheight);
+      ctx.globalAlpha = 1;
       setstate('playing');
     }
   }
 
-  function upload() {
-    var head = /^data:image\/(png|jpeg);base64,/,
-        data = '';
-
-    setstate('sending');
-    data = canvas.toDataURL('image/jpeg', 0.9).replace(head, '');
-    console.log(email);
-
-    $.ajax({
-          type: "POST",
-          url: "http://mandrillapp.com/api/1.0/messages/send.json",
-          data: {
-            'key': 'B6bSMmQ85PvY1CIAOws09Q' ,
-            'message': {
-              'from_email': 'sohil@motwanijadeja.org',
-              'to': [
-                  {
-                    'email': email,
-                    'name': '',
-                    'type': 'to'
-                  }
-                ],
-              'autotext' : 'true',
-              'subject' : 'Thank You : Here is your image',
-              'html' : 'YOUR EMAIL CONTENT HERE! YOU CAN USE HTML!',
-              'images': [
-                {
-                    "type": "image/jpeg",
-                    "name": "ClickedImage",
-                    "content": data
-                }
-              ]
-              }
-            }
-        });
-    console.log("You are Here!!!");
+    function downloadCanvas(link, canvasId, filename) {
+    link.href = document.getElementById(canvasId).toDataURL();
+    link.download = filename;
     setstate('uploaded');
     reshoot();
-    } 
+    }
 
   
  function setstate(newstate) {
@@ -138,23 +110,20 @@
     localStorage.interactionphotos += ' '+ name;
   }
 
-function validateEmail(email) {
-    var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
-    return re.test(email);
-}
-
   /* Event Handlers */
 
   video.addEventListener('play', function(ev){
     if (!streaming) {
-      console.log(video.clientHeight);
+      
       finalheight = video.clientHeight / (video.clientWidth/width);
       video.setAttribute('width', width);
       video.setAttribute('height', finalheight);
+
       canvas.width = width;
       canvas.height = finalheight;
-      ctx.drawImage(img, 590 - imgwidth, 440 - imgheight, imgwidth, imgheight);
-      streaming = true;
+      ctx.globalAlpha = 0.6;
+      ctx.drawImage(img, 0, 0);
+      ctx.globalAlpha = 1;
       vidcontainer.classname = 'playing';
     }
   }, false);
@@ -164,44 +133,32 @@ function validateEmail(email) {
       ev.preventDefault();
     }
     if (ev.which === 32) {
-      // if (state === 'intro') {
-      //   setstate('playing');
-      //   init();
-      // } else {
         setstate('reviewing');
         takepicture();
-      // }
     }
   },false);
 
-  resetbutton.addEventListener('click', function(ev){
+  clickbutton.addEventListener('click', function(ev){
+    if (state === 'playing') {
+        setstate('reviewing');
+        takepicture();
+    }
+    ev.preventDefault();
+  }, false);
+
+  document.getElementById('uploadbutton').addEventListener('click', function() {
+
     if (state === 'reviewing') {
+    downloadCanvas(this, 'canvas', 'selfie.png');
+    }
+    ev.preventDefault();
+  }, false);
+
+    document.getElementById('resetbutton').addEventListener('click', function() {
+      if (state === 'reviewing') {
       reshoot();
     }
     ev.preventDefault();
-  }, false);
-
-  clickbutton.addEventListener('click', function(ev){
-    if (state === 'playing') {
-      email = document.getElementById("emailvalue").value;
-      if(validateEmail(email))
-      {
-        setstate('reviewing');
-        takepicture();
-      }
-      else
-      {
-        //alert('Enter Valid Email Address');
-      }
-    }
-    ev.preventDefault();
-  }, false);
-
-  uploadbutton.addEventListener('click', function(ev){
-    if (state === 'reviewing') {
-      upload();
-    }
-    ev.preventDefault();
-  }, false);
+    }, false);
 
 })();
