6bd3bbc4de6c717dd0463cae9c1e30a5033298e0
jimish <jimishparekh123@outlook.com>
2015-12-16 23:49:33 +0530

    Adding drop.js for hover

---
+++ js/drop-custom.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+	var learnerDrop, makerDrop, researcherDrop, coderDrop, coachDrop, volunteerDrop;
+	learnerDrop = new Drop({
+	    target: document.querySelector('.learner-ico'),
+	    content: 'Learner',
+	    position: 'top center',
+	    openOn: 'hover'
+	  });
+	makerDrop = new Drop({
+	    target: document.querySelector('.maker-ico'),
+	    content: 'Maker',
+	    position: 'top center',
+	    openOn: 'hover'
+	  });
+	researcherDrop = new Drop({
+	    target: document.querySelector('.researcher-ico'),
+	    content: 'Researcher',
+	    position: 'top center',
+	    openOn: 'hover'
+	  });
+	coderDrop = new Drop({
+	    target: document.querySelector('.coder-ico'),
+	    content: 'Coder',
+	    position: 'top center',
+	    openOn: 'hover'
+	  });
+	coachDrop = new Drop({
+	    target: document.querySelector('.coach-ico'),
+	    content: 'Coach',
+	    position: 'top center',
+	    openOn: 'hover'
+	  });
+	volunteerDrop = new Drop({
+	    target: document.querySelector('.volunteer-ico'),
+	    content: 'Volunteer',
+	    position: 'top center',
+	    openOn: 'hover'
+	  });
+});
